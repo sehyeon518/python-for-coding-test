a969d53ba6c7b3171d86e56046a86d73beb850e3
sehyeon518 <84698896+sehyeon518@users.noreply.github.com>
2022-07-20 15:18:48 +0900

    1이 될 때까지

--- chapter-03/4.py
+++ chapter-03/4.py
@@ -11,3 +11,18 @@ while N > 1:
     N -= 1
     count += 1
 print(count)
+
+# 3-4.py 답안 예시
+N, K = map(int, input().split())
+count = 0
+
+while True:
+	target = (N // K) * K
+	count += (N - target)
+	N = target
+	if N < K:
+		break
+	count += 1
+	N //= K
+count += (N-1)
+print(count)
