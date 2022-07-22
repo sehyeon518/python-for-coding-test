8e0138d96dd882b227c9522eb68c0d28971acb2f
sehyeon518 <84698896+sehyeon518@users.noreply.github.com>
2022-07-22 11:46:43 +0900

    Create 10.py

---
+++ chapter-05/10.py
@@ -0,0 +1,44 @@
+# 5-10.py
+N, M = map(int, input().split())
+
+graph = []
+for n in range(N):
+    graph.append(list(map(int, input())))
+
+def dfs(x, y):
+    if x <= -1 or x >= N or y <= -1 or y >= M:
+        return False
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x, y-1)
+        dfs(x, y+1)
+        return True
+    return False
+
+result = 0
+for n in range(N):
+    for m in range(M):
+        if dfs(n, m):
+            result += 1
+
+print(result)
+'''
+15 14
+00000111100000
+11111101111110
+11011101101110
+11011101100000
+11011111111111
+11011111111100
+11000000011111
+01111111111111
+00000000011111
+01111111111000
+00011111111000
+00000001111000
+11111111110011
+11100011111111
+11100011111111
+'''
